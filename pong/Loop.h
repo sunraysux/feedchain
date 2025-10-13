34c7c8dec37834c52f12e56e52dc52ca2870c777
Nomerrrok <Artem4ekEH@yandex.ru>
2025-10-13 21:33:23 +0700

    передвинул что-то

--- pong/Loop.h
+++ pong/Loop.h
@@ -1,4 +1,8 @@
-﻿void terraloop()
+﻿
+
+
+
+void terraloop()
 {
 	InputAssembler::IA(InputAssembler::topology::triList);
 	Blend::Blending(Blend::blendmode::alpha, Blend::blendop::add);
@@ -195,44 +199,3 @@ void Looppause() {
 
 }
 
-
-bool initgame = false;
-bool initmenu = false;
-void drawWorld()
-{
-
-	switch (gameState)
-	{
-	case gameState_::MainMenu:
-		if (!initmenu) {
-			
-			Textures::LoadTextureFromFile(30, L"Debug/menu.png");
-			Textures::LoadTextureFromFile(31, L"Debug/start.png");
-			Textures::LoadTextureFromFile(32, L"Debug/exit.png");
-			Textures::LoadTextureFromFile(10, L"Debug/i.jpg");
-			Textures::ReadTextureToCPU(10);
-			initmenu = true;
-		}
-		ShowCursor(true);
-		StartMenu();
-		// gameState = gameState_::game;
-		break;
-
-	case gameState_::game:
-		if (!initgame) {
-			InitGame();
-			initgame = true;
-		}
-		ShowCursor(false);
-		Loop();
-		break;
-	case gameState_::pause:
-		if (!initgame) {
-			InitGame();
-			initgame = true;
-		}
-		ShowCursor(false);
-		Looppause();
-		break;
-	}
-}
--- pong/main.cpp
+++ pong/main.cpp
@@ -37,8 +37,9 @@ float length(float x1, float y1, float x2, float y2)
 {
     return sqrt(pow(x1 - x2, 2) + pow(y1 - y2, 2));
 }
-
+#include "Interface.h"
 #include "Loop.h"
+#include "Game.h"
 #define MAX_LOADSTRING 100
 // current instance
 WCHAR szTitle[MAX_LOADSTRING];                  // The title bar text
